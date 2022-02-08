f52e46ed39886554dfcf2a555ec10e113bd9a343
Robertisimwe <robertishimwe0@gmail.com>
2022-02-08 20:25:12 +0200

    add view post functionality

--- javaScript/blogFetch.js
+++ javaScript/blogFetch.js
@@ -1,7 +1,7 @@
 
 let viewPost=(postId)=>{
-  console.log("mee:"+postId);
-  console.log("hello world");
+//   console.log("mee:"+postId);
+//   console.log("hello world");
 let Posting = localStorage.getItem(postId);
 localStorage.setItem("currentPost",Posting)
 localStorage.setItem("currentPostId",JSON.stringify(postId))
