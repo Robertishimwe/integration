04427b4ee0a08c5827b528a2444a2a0ff5b2cf0f
Robertisimwe <robertishimwe0@gmail.com>
2022-02-08 18:25:56 +0200

    protected private route 2

--- javaScript/control_panel.js
+++ javaScript/control_panel.js
@@ -1,2 +1,11 @@
 const permision = sessionStorage.getItem("LoggedIn")
-if(JSON.parse(permision).Role !== "admin") return location = "../html/login.html";
+if(!permision){
+  location = "../html/login.html"
+}
+else if(JSON.parse(permision).Role == "admin"){
+    alert("welcome to admin panel")
+}else{
+    // location = "../html/login.html";
+    console.log(permision)
+    setTimeout(() => { location = "../html/login.html"}, 3000);
+}
